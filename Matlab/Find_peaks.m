294ff61e9d4011354cea4795c5751a42ce09a74d
fredasu <feisu9@gmail.com>
2024-03-24 16:10:41 -0400

    less manual adjusting needed for findPeaks

--- Matlab/Find_peaks.m
+++ Matlab/Find_peaks.m
@@ -3,10 +3,27 @@ data = rawdata.data;
 x = data(:, 3);
 y = data(:, 4);
 z = data(:, 5);
-%[pks,locs] = 
-findpeaks(-x,'SortStr', 'descend', 'Npeaks',100)
-%hold on
-%figure(1)
-%plot(x)
-%plot(locs, pks, 'o')
-%hold off
+
+curr = x;   %which column of data to look at
+[rawPk, rawLoc] = findpeaks(curr);
+diff = [];
+dist = [];
+for i = 1:(length(rawPk) - 1)   %filtering
+    height = abs(rawPk(i+1) - rawPk(i));    %in case values are negative
+    if (height > 0.01)  %sensitivity: the smaller the value, the more peaks will be found
+        diff(end+1) = height;
+        dist(end+1) = rawLoc(i+1) - rawLoc(i);  %no abs because index always positive
+    end
+end
+
+prominence = mean(diff);
+peakDist = mean(dist);
+
+[pks,locs] = findpeaks(curr,'MinPeakDistance', peakDist, 'MinPeakProminence', prominence);
+[valleys,vLocs] = findpeaks(-curr,'MinPeakDistance', peakDist, 'MinPeakProminence', prominence);
+hold on
+figure(1)
+plot(curr)
+plot(locs, pks, 'o')
+plot(vLocs, -valleys, 'x')
+hold off
